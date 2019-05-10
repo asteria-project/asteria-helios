22d06e2a4a796397333ccd26c60e41038f20db3c
Pascal ECHEMANN <pascal@wooz.io>
2019-05-10 15:36:32 +0200

    Initial commit
    Initial commit of the Helios module.

---
+++ src/com/asteria/helios.index.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+/*!
+ * Module dependencies.
+ * Please maintain package and alphabetical order!
+ */
+
+ //--> com/asteria/helios
---
+++ utils/types/helios.d.ts
@@ -0,0 +1,10 @@
+/*!
+ * Asteria Helios Node Module
+ * Copyright(c) 2019 Pascal ECHEMANN
+ * Apache 2.0 Licensed
+ * This is a part of the Asteria Project: <https://github.com/asteria-project>
+ */
+
+declare module "asteria-helios" {
+
+/*PLACEHOLDER*/}
