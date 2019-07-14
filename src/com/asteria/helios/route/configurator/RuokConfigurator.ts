61867696c994e8063ae6ed1c1f25000e809a0edb
Pascal ECHEMANN <pechemann@gmail.com>
2019-07-14 10:32:14 +0200

    API improvement
    Return a HeliosData object when the ruok route is called.

--- src/com/asteria/helios/route/configurator/RuokConfigurator.ts
+++ src/com/asteria/helios/route/configurator/RuokConfigurator.ts
@@ -5,6 +5,8 @@ import { HeliosRouter } from '../HeliosRouter';
 import { HeliosRoute } from '../HeliosRoute';
 import { HeliosRouterLogUtils } from '../../util/route/HeliosRouterLogUtils';
 import { AbstractHeliosRouteConfigurator } from './AbstractHeliosRouteConfigurator';
+import { HeliosData } from 'asteria-eos';
+import { HeliosDataBuilder } from '../../util/builder/HeliosDataBuilder';
 
 /**
  * The <code>RuokConfigurator</code> class is the <code>HeliosRouteConfigurator</code> implementation to declare the
@@ -23,9 +25,12 @@ export class RuokConfigurator extends AbstractHeliosRouteConfigurator implements
      * @inheritdoc
      */
     public createRoute(router: HeliosRouter, context: HeliosContext): void {
+        const pathPattern: string = 'GET /ruok';
+        const message: string = 'I\'m still alive!';
         router.getRouter().get(HeliosRoute.RUOK, (req: Request, res: Response) => {
-            HeliosRouterLogUtils.logRoute(req, 'GET /ruok');
-            res.send('I\'m still alive!');
+            HeliosRouterLogUtils.logRoute(req, pathPattern);
+            const result: HeliosData<string> =  HeliosDataBuilder.build<string>(context.getId(), message);
+            res.send(result);
         });
        this.routeAdded(HeliosRoute.RUOK);
     }
